700282444338fdc2337fa8d3ac64b6cb5c01b161
wasongapaul5 <wasongapaul5@gmail.com>
2019-09-30 10:55:15 +0300

    creat a hover function to the images

--- css/scripts.js
+++ css/scripts.js
@@ -4,26 +4,26 @@ $(document).ready(function(){
         $(this).find(".project").toggle(400);
     });
 
-    $(".click0").click(function(){
-        $(".hide0").slideToggle(500);
-        $(".show0").slideToggle(1200);
-    });
+    // $(".click0").click(function(){
+    //     $(".hide0").slideToggle(500);
+    //     $(".show0").slideToggle(1200);
+    // });
 
-    $(".click1").click(function(){
-        $(".hide1").slideToggle(100);
-        $(".show1").slideToggle(1200);
-    });
+    // $(".click1").click(function(){
+    //     $(".hide1").slideToggle(100);
+    //     $(".show1").slideToggle(1200);
+    // });
     
-    $(".click2").click(function(){
-        $(".hide2").slideToggle(500);
-        $(".show2").slideToggle(1200);
-    }); 
+    // $(".click2").click(function(){
+    //     $(".hide2").slideToggle(500);
+    //     $(".show2").slideToggle(1200);
+    // }); 
 
-    $("button").click(function() {
-        var user = document.getElementById("names").Value;
-        alert('Thank you'  +  'we have recieved your comment successfully' + '!');
-        // party.preventDefault();
-    });
+    // $("button").click(function() {
+    //     var user = document.getElementById("names").Value;
+    //     alert('Thank you'  +  'we have recieved your comment successfully' + '!');
+    //     // party.preventDefault();
+    // });
 
 });
 
