bdf6d4ed06e24843ce9f4b8d195c94561c3d106e
wasongapaul5 <wasongapaul5@gmail.com>
2019-09-30 10:42:54 +0300

    add height to the id comment

--- css/scripts.js
+++ css/scripts.js
@@ -5,27 +5,25 @@ $(document).ready(function(){
     });
 
     $(".click0").click(function(){
-        $(".hide0").slideToggle(1000);
+        $(".hide0").slideToggle(500);
         $(".show0").slideToggle(1200);
     });
 
     $(".click1").click(function(){
-        $(".hide1").slideToggle(1000);
-        $("show1").slideToggle(1200);
+        $(".hide1").slideToggle(100);
+        $(".show1").slideToggle(1200);
     });
     
     $(".click2").click(function(){
-        $(".hide2").slideToggle(1000);
-        $("show2").slideToggle(1200);
-    });
+        $(".hide2").slideToggle(500);
+        $(".show2").slideToggle(1200);
+    }); 
 
-    $("button").click(function(party) {
+    $("button").click(function() {
         var user = document.getElementById("names").Value;
         alert('Thank you'  +  'we have recieved your comment successfully' + '!');
-        party.preventDefault();
+        // party.preventDefault();
     });
 
-   $(document).ready(function(){
-     
-   })
-    });
+});
+
